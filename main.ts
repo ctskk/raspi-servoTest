1d20cb5e454d3268fe6c8a3e6b71cc247d7ed828
ctskk <ctskk.1974@gmail.com>
2016-05-25 22:39:35 +0900

    add Servo class.

--- main.ts
+++ main.ts
@@ -24,27 +24,8 @@ var websocket = io.listen(server);
 //接続ソケット管理用配列
 var sockets = {};
 
-//サーボの初期角度
-var servo_angle : number = 0;
-
-//サーボ制御用GPIO PIN番号
-let PIN_NUMBER : number = 18;
-
-//*
-//WiringPiの初期化
-if(wiringpi.wiringPiSetupGpio() == -1)
-{
-    console.log("[SRV] Setup error.");
-}
-else
-{
-    console.log("[SRV] GPIO setup ok.");
-}
-wiringpi.pinMode(PIN_NUMBER, wiringpi.PWM_OUTPUT);
-wiringpi.pwmSetMode(wiringpi.PWM_MODE_MS);
-wiringpi.pwmSetClock(400);
-wiringpi.pwmSetRange(1024);
-//*/
+//サーボクラスの初期化
+var servo : Servo = new Servo();
 
 //クライアントからSocket接続があった場合の処理
 websocket.on('connection', function(socket) {
@@ -61,46 +42,86 @@ websocket.on('connection', function(socket) {
 
     //startメッセージの処理
     socket.on('start', function() {
-        servo_angle = 0;
-        setServoAngle(0);
-        ack(websocket, servo_angle);
+        servo.setServoAngle(0);
+        ack();
     });
 
     //stopメッセージの処理
     socket.on('stop', function() {
-        servo_angle = 0;
-        setServoAngle(0);
-        ack(websocket, servo_angle);
+        servo.setServoAngle(0);
+        ack();
     });
 
     //setメッセージの処理
     socket.on('set', function(angle) {
-        servo_angle = angle;
-        setServoAngle(servo_angle);
-        ack(websocket, servo_angle);
+        servo.setServoAngle(angle);
+        ack();
     });
 
     //moveメッセージの処理
     socket.on('move', function(angle) {
-        servo_angle += angle;
-        setServoAngle(servo_angle);
-        ack(websocket, servo_angle);
+        servo.addServoAngle(angle);
+        ack();
     });
 
 });
 
-//サーボ角度を設定する
-function setServoAngle(angle : number) {
-    const st =  35;
-    const ed = 125;
-    let step = Math.ceil(((ed - st) / 180) * angle);
-    var num : number = st + step;
-    console.log('[SRV] STP:' + num);
-    wiringpi.pwmWrite(PIN_NUMBER, num);
+//ACK処理
+function ack() : void {
+    console.log('[SRV] ACK:' + servo.angle);
+    websocket.sockets.emit('ack', servo.angle);
 }
 
-//ACK処理
-function ack(io, angle : number) : void {
-    console.log('[SRV] ACK:' + angle);
-    io.sockets.emit('ack', angle);
+/**
+ * サーボ制御クラス
+ */
+class Servo {
+    
+    //GPIO制御用に使用するPIN番号
+    private pinNumber : number;
+    //サーボ角度
+    private servo_angle : number;
+    
+    //コンストラクタ
+    constructor(pinNumber : number = 18, clock : number = 400, range : number = 1024) {
+        
+        this.pinNumber = pinNumber;
+        this.servo_angle = 0;
+        
+        //WiringPiの初期化
+        if(wiringpi.wiringPiSetupGpio() == -1)
+        {
+            console.log("[SRV] Setup error.");
+        }
+        else
+        {
+            console.log("[SRV] GPIO setup ok.");
+        }
+        wiringpi.pinMode(this.pinNumber, wiringpi.PWM_OUTPUT);
+        wiringpi.pwmSetMode(wiringpi.PWM_MODE_MS);
+        wiringpi.pwmSetClock(clock);
+        wiringpi.pwmSetRange(range);
+    }
+    
+    get angle() : number { return this.servo_angle; }
+    
+    //サーボ角度を増減させる
+    public addServoAngle(angle : number) : void {
+        var newVal : number = this.servo_angle += angle;
+        if (newVal <   0) { this.servo_angle =   0; }
+        if (newVal > 180) { this.servo_angle = 180; }
+        this.setServoAngle(newVal);
+    }
+    
+    //サーボ角度を設定する
+    public setServoAngle(angle : number) : void {
+        this.servo_angle = angle;
+        const st =  35;
+        const ed = 118;
+        let step = Math.ceil(((ed - st) / 180) * this.servo_angle);
+        var num : number = st + step;
+        console.log('[SRV] STP:' + num);
+        wiringpi.pwmWrite(this.pinNumber, num);
+    }
+
 }
